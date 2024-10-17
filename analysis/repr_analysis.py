95023296c92d903722c397718bf68c350ae18866
DiegoD <doimo.diego@gmail.com>
2024-10-17 09:47:38 +0200

    u

--- analysis/repr_analysis.py
+++ analysis/repr_analysis.py
@@ -1,38 +1,23 @@
 from collections import defaultdict
-import torch
-from dadapy._cython import cython_overlap as c_ov
-from pairwise_distances import compute_distances
 import sys
 import numpy as np
 import pickle
-
 import os
 import argparse
-from dadapy import data
-from sklearn.metrics import adjusted_mutual_info_score, adjusted_rand_score
-from utils import return_label_overlap, analyze
-
-
-rng = np.random
+from utils import analyze
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Finetune a transformers model on a causal language modeling task"
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--layer_path",
+        type=str,
+        default="analysis/test_mask_200.npy",
     )
     parser.add_argument(
-        "--finetuned_mode",
+        "--mask_path",
         type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "--pretrained_mode",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "--mask_dir",
-        type=str,
+        default="analysis/test_mask_200.npy",
     )
     parser.add_argument(
         "--model_name",
@@ -46,31 +31,13 @@ def parse_args():
         help="Where to store the final model.",
     )
     parser.add_argument(
-        "--epochs",
-        type=int,
-    )
-    parser.add_argument(
         "--eval_dataset",
         type=str,
-    )
-    parser.add_argument(
-        "--samples_subject",
-        type=int,
-        default=200,
     )
     parser.add_argument(
         "--num_shots",
         type=int,
         default=None,
-    )
-    parser.add_argument(
-        "--ckpt",
-        type=int,
-        default=None,
-    )
-    parser.add_argument(
-        "--do_with_steps",
-        action="store_true",
     )
     args = parser.parse_args()
     return args
@@ -78,11 +45,6 @@ def parse_args():
 
 # **************************************************************************
 args = parse_args()
-
-assert args.finetuned_mode is None or args.pretrained_mode is None
-
-# base path
-base_dir = "/orfeo/cephfs/scratch/area/ddoimo/open/geometric_lens/repo/results"
 
 if args.model_name in ["llama-3-8b", "mistral-1-7b"]:
     nlayers = 34
@@ -102,11 +64,7 @@ print("analyzing model:", args.model_name)
 
 # dataset to analyze
 if args.eval_dataset == "test":
-    dataset_mask = np.load(f"./analysis/test_mask_200.npy")
-    is_balanced = f"_balanced200"
-elif args.eval_dataset == "dev+validation":
-    mask_dir = args.mask_dir
-    dataset_mask = np.load(f"./analysis/dev+validation_mask_20.npy")
+    dataset_mask = np.load(args.mask_path)
 else:
     assert False, "dataset misspecified"
 
@@ -115,104 +73,39 @@ overlaps = defaultdict(list)
 clusters = defaultdict(list)
 intrinsic_dim = defaultdict(list)
 
+args.results_path += f"/pretrained/{args.model_name}"
+os.makedirs(args.results_path, exist_ok=True)
 
-if args.finetuned_mode is not None:
-    print("finetuned_mode:", args.finetuned_mode)
-    args.results_path += f"/finetuned/{args.model_name}"
-    os.makedirs(args.results_path, exist_ok=True)
+# compute the statistics for all the shots unless a number of shot is given
+num_shots = np.arange(6)
+if args.num_shots is not None:
+    num_shots = [args.num_shots]
 
-    if args.do_with_steps:
-        ckpts = [1, 2, 3, 6, 12, 22, 42, 77, 144, 268]
-        for i_step, step in enumerate(ckpts[::-1]):
-            print("step:", step, f"{i_step+1}/{len(ckpts)}")
-            sys.stdout.flush()
+for shot in num_shots:
+    print("num_shot:", shot)
+    sys.stdout.flush()
+    name = f"{args.model_name}_mmlu_test_{shot}shot"
 
-            base_path = f"{base_dir}/finetuned_{args.finetuned_mode}/evaluated_{args.eval_dataset}/{args.model_name}/{args.epochs}epochs/10ckpts/step_{step}"
-            name = f"{args.model_name}_finetuned_{args.finetuned_mode}_epoch{args.epochs}_10ckpts_eval_{args.eval_dataset}{is_balanced}_0shot"
-
-            for layer in range(1, nlayers):
-                print("layer:", f"{layer}/{nlayers-1}")
-                sys.stdout.flush()
-
-                clusters, intrinsic_dim, overlaps = analyze(
-                    base_path,
-                    layer,
-                    dataset_mask,
-                    clusters,
-                    intrinsic_dim,
-                    overlaps,
-                    spec=f"step-{step}",
-                )
-    else:
-        ckpts = np.arange(args.epochs + 1)
-        if args.ckpt is not None:
-            ckpts = [args.ckpt]
-
-        for epoch in ckpts[::-1]:
-            base_path = f"{base_dir}/finetuned_{args.finetuned_mode}/evaluated_{args.eval_dataset}/{args.model_name}/{args.epochs}epochs/epoch_{epoch}"
-            name = f"{args.model_name}_finetuned_{args.finetuned_mode}_epoch{args.epochs}_eval_{args.eval_dataset}{is_balanced}_0shot"
-
-            print("epoch:", epoch)
-            sys.stdout.flush()
-
-            for layer in range(1, nlayers):
-                print("layer:", f"{layer}/{nlayers-1}")
-                sys.stdout.flush()
-
-                clusters, intrinsic_dim, overlaps = analyze(
-                    base_path,
-                    layer,
-                    dataset_mask,
-                    clusters,
-                    intrinsic_dim,
-                    overlaps,
-                    spec=f"ep-{epoch}",
-                )
-
-            with open(f"{args.results_path}/overlap_{name}.pkl", "wb") as f:
-                pickle.dump(overlaps, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-            with open(f"{args.results_path}/cluster_{name}.pkl", "wb") as f:
-                sys.stdout.flush()
-                pickle.dump(clusters, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-            with open(f"{args.results_path}/ids_{name}.pkl", "wb") as f:
-                pickle.dump(intrinsic_dim, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-elif args.pretrained_mode is not None:
-    print("pretrained mode:", args.pretrained_mode)
-    args.results_path += f"/pretrained/{args.model_name}"
-    os.makedirs(args.results_path, exist_ok=True)
-    num_shots = np.arange(6)
-    if args.num_shots is not None:
-        num_shots = [args.num_shots]
-
-    for shot in num_shots[::-1]:
-        # base_path = f"{base_dir}/evaluated_{args.eval_dataset}/{args.pretrained_mode}/{args.model_name}/{shot}shot"
-        name = f"{args.model_name}_eval_{args.eval_dataset}{is_balanced}_{shot}shot"
-        print("num_shot:", shot)
+    for layer in range(1, nlayers):
+        print("layer:", f"{layer}/{nlayers-1}")
         sys.stdout.flush()
 
-        for layer in range(1, nlayers):
-            print("layer:", f"{layer}/{nlayers-1}")
-            sys.stdout.flush()
+        clusters, intrinsic_dim, overlaps = analyze(
+            args.layer_path,
+            layer,
+            dataset_mask,
+            clusters,
+            intrinsic_dim,
+            overlaps,
+            spec=f"{shot}shot",
+        )
 
-            clusters, intrinsic_dim, overlaps = analyze(
-                args.layer_path,
-                layer,
-                dataset_mask,
-                clusters,
-                intrinsic_dim,
-                overlaps,
-                spec=f"{shot}shot",
-            )
+    with open(f"{args.results_path}/overlap_{name}.pkl", "wb") as f:
+        pickle.dump(overlaps, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-        with open(f"{args.results_path}/overlap_{name}.pkl", "wb") as f:
-            pickle.dump(overlaps, f, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(f"{args.results_path}/cluster_{name}.pkl", "wb") as f:
+        sys.stdout.flush()
+        pickle.dump(clusters, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-        with open(f"{args.results_path}/cluster_{name}.pkl", "wb") as f:
-            sys.stdout.flush()
-            pickle.dump(clusters, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-        with open(f"{args.results_path}/ids_{name}.pkl", "wb") as f:
-            pickle.dump(intrinsic_dim, f, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(f"{args.results_path}/ids_{name}.pkl", "wb") as f:
+        pickle.dump(intrinsic_dim, f, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ analysis/utils.py
@@ -0,0 +1,199 @@
+from collections import Counter
+from dadapy._cython import cython_overlap as c_ov
+import numpy as np
+import warnings
+import torch
+import pickle
+from extraction.pairwise_distances import compute_distances
+from sklearn.metrics import adjusted_rand_score
+from dadapy import data
+from collections import Counter
+import math
+
+
+def return_data_overlap(indices_base, indices_other, k=30, subjects=None):
+
+    assert indices_base.shape[0] == indices_other.shape[0]
+    ndata = indices_base.shape[0]
+
+    overlaps_full = c_ov._compute_data_overlap(
+        ndata, k, indices_base.astype(int), indices_other.astype(int)
+    )
+
+    overlaps = np.mean(overlaps_full)
+    if subjects is not None:
+        overlaps = {}
+        for subject in np.unique(subjects):
+            mask = subject == subjects
+            overlaps[subject] = np.mean(overlaps_full[mask])
+
+    return overlaps
+
+
+def _label_imbalance_helper(labels, k, class_fraction):
+    if k is not None:
+        max_k = k
+        k_per_sample = np.array([k for _ in range(len(labels))])
+
+    k_per_class = {}
+    class_count = Counter(labels)
+    # potentially overwrites k_per_sample
+    if class_fraction is not None:
+        for label, count in class_count.items():
+            class_k = int(count * class_fraction)
+            k_per_class[label] = class_k
+            if class_k == 0:
+                k_per_class[label] = 1
+                warnings.warn(
+                    f" max_k < 1 for label {label}. max_k set to 1.\
+                    Consider increasing class_fraction.",
+                    stacklevel=2,
+                )
+        max_k = max([k for k in k_per_class.values()])
+        k_per_sample = np.array([k_per_class[label] for label in labels])
+
+    class_weights = {label: 1 / count for label, count in class_count.items()}
+    sample_weights = np.array([class_weights[label] for label in labels])
+
+    return k_per_sample, sample_weights, max_k
+
+
+def return_label_overlap(
+    dist_indices,
+    labels,
+    k=None,
+    avg=True,
+    class_fraction=None,
+    weighted=True,
+):
+    k_per_sample, sample_weights, max_k = _label_imbalance_helper(
+        labels, k, class_fraction
+    )
+    # print(k_per_sample, sample_weights, max_k)
+    assert len(labels) == dist_indices.shape[0]
+
+    neighbor_index = dist_indices[:, 1 : max_k + 1]
+
+    ground_truth_labels = np.repeat(np.array([labels]).T, repeats=max_k, axis=1)
+
+    overlaps = np.equal(np.array(labels)[neighbor_index], ground_truth_labels)
+
+    if class_fraction is not None:
+        nearest_neighbor_rank = np.arange(max_k)[np.newaxis, :]
+        # should this overlap entry be discarded?
+        mask = nearest_neighbor_rank >= k_per_sample[:, np.newaxis]
+        # mask out the entries to be discarded
+        overlaps[mask] = False
+
+    overlaps = overlaps.sum(axis=1) / k_per_sample
+    if avg and weighted:
+        overlaps = np.average(overlaps, weights=sample_weights)
+    elif avg:
+        overlaps = np.mean(overlaps)
+
+    return overlaps
+
+
+def analyze(
+    base_path,
+    layer,
+    dataset_mask,
+    clusters,
+    intrinsic_dim,
+    overlaps,
+    spec="",
+    k=16,
+    z=1.6,
+    class_fraction=0.3,
+):
+
+    activations = torch.load(f"{base_path}/l{layer}_target.pt")
+    activations = activations.to(torch.float64).numpy()
+    print("dataset_size:", activations.shape[0])
+
+    with open(f"{base_path}/statistics_target.pkl", "rb") as f:
+        stats = pickle.load(f)
+
+    subjects = stats["subjects"]
+    subjects_to_int = {sub: i for i, sub in enumerate(np.unique(subjects))}
+    subj_label = np.array([subjects_to_int[sub] for sub in subjects])
+
+    letters = stats["answers"]
+    letters_to_int = {letter: i for i, letter in enumerate(np.unique(letters))}
+    letter_label = np.array([letters_to_int[sub] for sub in letters])
+
+    # select up to 200 points per class.
+    activations = activations[dataset_mask]
+    subj_label = subj_label[dataset_mask]
+    letter_label = letter_label[dataset_mask]
+
+    # remove identical points
+    _, base_idx, _ = np.unique(
+        activations, axis=0, return_index=True, return_inverse=True
+    )
+    indices = np.sort(base_idx)
+    activations = activations[indices]
+    subj_label = subj_label[indices]
+    letter_label = letter_label[indices]
+    print("dataset_size after mask and prune:", activations.shape[0])
+
+    # ***********************************************************************
+
+    maxk = 1000
+    assert indices.shape[0] > maxk, (indices.shape[0], maxk)
+    # distances_base, dist_index_base, _, _ = compute_distances(
+    #     X=activations,
+    #     n_neighbors=maxk + 1,
+    #     n_jobs=1,
+    #     working_memory=2048,
+    #     range_scaling=2048,
+    #     argsort=False,
+    # )
+
+    d = data.Data(coordinates=activations)
+    ids, _, _ = d.return_id_scaling_gride(range_max=100)
+    dist_index_base = d.dist_indices
+
+    # this sets the kNN order of the density estimator consistent with
+    # the one used to compute the ID with gride
+    id_index = int(math.log2(k))
+
+    d.set_id(ids[id_index])
+    intrinsic_dim[f"ids-{spec}"].append(ids)
+    d.compute_density_kNN(k=k)
+    assignment = d.compute_clustering_ADP(Z=z, halo=False)
+
+    # number of clusters found
+    clusters[f"nclus-{spec}-z{z}-k{k}"] = d.N_clusters
+
+    # ARI with subjects
+    clusters[f"subjects-ari-{spec}-z{z}-k{k}"] = adjusted_rand_score(
+        assignment, subj_label
+    )
+
+    # ARI with answers
+    clusters[f"letters-ari-{spec}-z{z}-k{k}"] = adjusted_rand_score(
+        assignment, letter_label
+    )
+
+    # halo computation
+    assignment_core = d.compute_clustering_ADP(Z=z, halo=True)
+    clusters[f"core_point_fraction-{spec}"] = np.sum(assignment_core != -1) / len(
+        assignment_core
+    )
+
+    # overlap subjects
+    overlaps[f"subjects-{spec}-{class_fraction}"] = return_label_overlap(
+        dist_indices=dist_index_base,
+        labels=list(subj_label),
+        class_fraction=class_fraction,
+    )
+
+    # overlap letters
+    overlaps[f"letters-{spec}-{class_fraction}"] = return_label_overlap(
+        dist_indices=dist_index_base,
+        labels=list(letter_label),
+        class_fraction=class_fraction,
+    )
+
+    return clusters, intrinsic_dim, overlaps
